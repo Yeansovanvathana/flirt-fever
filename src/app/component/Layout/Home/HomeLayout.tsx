b7b393f68d0cfd7afbdde74ace8c6012235e929f
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-08-23 16:49:25 +0700

    solve ssr

--- src/app/component/Layout/Home/HomeLayout.tsx
+++ src/app/component/Layout/Home/HomeLayout.tsx
@@ -1,27 +1,32 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { People } from "@/utils/People";
-import TinderCard from "react-tinder-card";
+import dynamic from "next/dynamic";
+
+const DynamicTinderCard = dynamic(() => import("react-tinder-card"), {
+  ssr: false, // Prevent SSR
+});
 
 const HomeLayout = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      <div className="mt-20 flex justify-center items-center bg-green-400">
+      <div className="mt-20 relative flex justify-center items-center bg-green-400">
         {People.map((character, index) => (
-          <TinderCard
-            preventSwipe={["up", "down"]}
-            key={character.id}
-            className="absolute"
-          >
+          <DynamicTinderCard preventSwipe={["up", "down"]} className="absolute">
             <div
               style={{ backgroundImage: `url(${character.img})` }}
               className="relative w-64 p-2 max-w-sm h-96 rounded-xl shadow-lg bg-cover bg-center"
             >
-              <h3 className="absolute text-white font-medium bottom-3">
-                {character.name}
-              </h3>
+              <div className="absolute text-white font-medium bottom-3">
+                <h3>{character.name}</h3>
+                <div>
+                  {/* {character.interests.map((interest) => () => {
+                    return <p>{interest}</p>;
+                  }} */}
+                </div>
+              </div>
             </div>
-          </TinderCard>
+          </DynamicTinderCard>
         ))}
       </div>
     </div>
