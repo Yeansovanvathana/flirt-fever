554dbd49a3af90e4557484cb2e7525364f6aef31
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2023-08-28 22:39:50 +0700

    Update Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     <main className="flex min-h-screen">
       <div>
         <Navbar />
-        {/* <LoadingPage /> */}
         <FlirtCard />
       </div>
     </main>
